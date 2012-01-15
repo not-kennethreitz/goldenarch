b3cfdd99297b015e5999afd90465b4e595103325
Kenneth Reitz <me@kennethreitz.com>
2012-01-15 02:55:27 -0500

    static makes that easier

--- goldenarch/core.py
+++ goldenarch/core.py
@@ -8,25 +8,15 @@ Serves crap. Fast.
 """
 
 import os
+import sys
 
-from werkzeug.wrappers import Request
-from werkzeug.wsgi import SharedDataMiddleware
-
+import static
 
 PORT = os.environ.get('PORT', 8000)
-STATIC_DIR = os.environ.get('STATIC_DIR', '.')
+STATIC_DIR = sys.argv[1]
 
-class GoldenArch(object):
-    def __init__(self):
-        super(GoldenArch, self).__init__()
 
-    def wsgi_app(self, environ, start_response):
-        request = Request(environ)
-        response = self.dispatch_request(request)
-        return response(environ, start_response)
-
-    def __call__(self, environ, start_response):
-        return self.wsgi_app(environ, start_response)
+app = static.Cling(STATIC_DIR)
 
 
 def cli():
@@ -38,18 +28,9 @@ def cli():
         '-w 16 -k gevent -t 2 '
         '--name goldenarch'
     ).format(port=PORT)
+
     os.system(cmd)
 
-
-
-app = GoldenArch()
-
-app.wsgi_app = SharedDataMiddleware(
-    app.wsgi_app,
-    {
-        '/': os.path.join(STATIC_DIR)
-    }
-)
 
 if __name__ == '__main__':
     cli()
