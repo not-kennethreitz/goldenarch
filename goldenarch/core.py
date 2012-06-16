515b0f91341b3a622ece70002f756de21a460159
Nick Stenning <nick@whiteink.com>
2012-06-16 15:03:24 -0400

    Actually pay attention to the commandline

--- goldenarch/core.py
+++ goldenarch/core.py
@@ -13,17 +13,17 @@ import sys
 import static
 
 PORT = os.environ.get('PORT', 8000)
-STATIC_DIR = sys.argv[1]
 
-
-app = static.Cling(STATIC_DIR)
-
+def app(static_dir):
+    return static.Cling(static_dir)
 
 def cli():
     print 'Serving crap. Fast.'
 
+    static_dir = sys.argv[1]
+
     argv = [
-        'gunicorn', 'goldenarch:app',
+        'gunicorn', 'goldenarch:app("{dir}")'.format(dir=static_dir),
         '-b', '0.0.0.0:{port}'.format(port=PORT),
         '-w', '16', '-k', 'gevent', '-t', '2',
         '--name', 'goldenarch'
